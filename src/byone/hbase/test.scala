09153bffa88dcb36f73cb77c62caf32f691af7c3
tjuliuyou <tjuliuyou@gmail.com>
2014-07-14 23:08:35 +0800

    json parser worked with small bugs

--- src/byone/hbase/test.scala
+++ src/byone/hbase/test.scala
@@ -2,14 +2,14 @@ package byone.hbase
 
 import byone.hbase.core.{Man}
 import java.lang.String
-import net.liftweb.json._
-import net.liftweb.json.JsonParser._
+import net.liftweb.json.Formats
+import net.liftweb.json.JsonParser.parse
 /**
  * Created by dream on 7/11/14.
  */
 object test {
-
-  def main(args: Array[String]){
+  //implicit formats = DefaultFormats
+  def main(args: Array[String]) {
 //    val pf = new ParseFilter()
 //    val x = pf.parseFilterString("PrefixFilter ('P') AND (SingleColumnValueFilter ('d','id',<,'binary:0005'))")
 //    val s = new Scan
@@ -17,11 +17,23 @@ object test {
 //    val man = new Man
 //    man.scanV(s,"uid").foreach(println)
 //    Conf.sc.stop()
-//    implicit formats = DefaultFormats
-//    case class Args(testitem: String)
+    implicit val formats = net.liftweb.json.DefaultFormats
+    //case class Range(rg: List[String])
+    //case class Items(it: List[String])
+    case class Agg(op: String, ar: List[String])
+    //case class Events(et: List[String])
+    //case class Groupby(gp:List[String])
+    case class Args(Range: List[String]
+                    ,Items: List[String]
+                    ,Events: Option [List[String]]
+                    ,Filter:  Option [String]
+                    ,Groupby: Option [List[String]]
+                    ,Aggres: Option[(String,List[String])])
+
     val source = scala.io.Source.fromFile("test/test.json").mkString
     println(source)
-   // val m = source.extract[Args]
-
+    val m = parse(source)
+    val testlist: List[Args] = m.children.map(_.extract[Args])
+    testlist.foreach(x => println(x))
   }
 }
