935411101f495f37e40222f9a842b7329ae8529e
tjuliuyou <tjuliuyou@gmail.com>
2014-08-18 10:11:43 +0800

    using twitter future instand of java execuders

--- src/main/scala/byone/hbase/util/DatePoint.scala
+++ src/main/scala/byone/hbase/util/DatePoint.scala
@@ -1,8 +1,8 @@
 package byone.hbase.util
 
 import java.text.SimpleDateFormat
-import org.apache.hadoop.hbase.client.{Result, Scan}
-import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+
+import org.apache.hadoop.hbase.client.Scan
 
 /**
  * Created by dream on 7/7/14.
